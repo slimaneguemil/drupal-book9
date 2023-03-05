8f1c60ee15144201e1b266cef4c1c46468d9811a
Lenovo <mzd33yyfte6pmqexeggzpp4wuygrf5fhxjdbiuakitqpoqjtwmgq>
2023-03-05 09:18:16 +0100

    init project

--- backend/web/sites/default/settings.php
+++ backend/web/sites/default/settings.php
@@ -826,3 +826,4 @@ $settings['config_sync_directory'] = '../config/sync';
 
  if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
    include $app_root . '/' . $site_path . '/settings.local.php';
+ }
